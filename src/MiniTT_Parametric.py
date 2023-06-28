386f02f1e0fef9604e6bd8a7493c44148915ac3c
Al Shahriar <al.shahriar@danfoss.com>
2023-06-28 18:37:30 -0400

    Data indexing, data cleaning and traing-test seperation is done. Data loaded into NN model also.

--- src/MiniTT_Parametric.py
+++ src/MiniTT_Parametric.py
@@ -1,12 +1,13 @@
-# -*- coding: utf-8 -*-
+# Author: Al Shahriar
 # Jun 21 2023
-# Modification_author: Al Shahriar
 
 # %% Load libraries
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import os
+
+import matplotlib.pyplot as plt
 
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
@@ -37,46 +38,59 @@ from tensorflow.keras.regularizers import l1, l2
 from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, EarlyStopping
 from tensorflow.keras.utils import plot_model
 
-# %%
-##########################################################  Data Cleaning    ########################################################################
 
-train_data_dir = "../training_data/training_batch_173.csv"
-test_data_dir = "../testing_data/testing_batch_173.csv"
+# %% Data Cleaning
 
-# # This locates any rows of duplicate data, which is not ideal for model training.
-# from pandas import read_csv
-# # define the location of the dataset
-# path = r".csv"
-# # load the dataset
-# df = read_csv(path, header=None)
-# # calculate duplicates
-# dups = df.duplicated()
-# # report if there are any duplicates
-# print(dups.any())
-# # list all duplicate rows
-# print(df[dups])
-# #If "False" appears, then no data duplicates are located.
+# Locations
+if 0:
+    train_data_dir = "../training_data/training_batch_173.csv"
+    test_data_dir = "../testing_data/testing_batch_173.csv"
+    read_method = 0
+else:
+    train_data_dir = "../training_data"
+    test_data_dir = "../testing_data"    
+    train_data_fname = "train_parameters.pkl"
+    test_data_fname = "test_parameters.pkl"
+    train_full_dir = os.path.join(train_data_dir, train_data_fname)
+    test_full_dir = os.path.join(test_data_dir, test_data_fname)
+    read_method  = 1
 
-###################################################################   Inputs  #####################################################################
-#Code Inputs:
-in_col=18 #Number of input columns
-out_col=3 #Number of output columns
+# Inputs
+in_col = 18 #Number of input columns
+out_col = 3 #Number of output columns
 
-##################################################################  Importing  ####################################################################
 
 #Import data
-train_data = np.loadtxt(r"../training_data/training_batch_173.csv", delimiter=",")
-test_data = np.loadtxt(r"../testing_data/testing_batch_173.csv", delimiter=",")
+if read_method==0:
+    train_data = np.loadtxt(train_data_dir, delimiter=",")
+    test_data = np.loadtxt(test_data_dir, delimiter=",")
+else:
+    train_data_df = pd.read_pickle(train_full_dir)
+    test_data_df = pd.read_pickle(test_full_dir)
+    train_data = train_data_df.to_numpy()
+    test_data = test_data_df.to_numpy()    
 
-#Training data
+# Removing duplicates
+# define the location of the dataset
+# calculate duplicates
+dups = train_data_df.duplicated()
+if dups.any():
+    print("Duplicates found on train data")
+    train_data_df = train_data_df.drop_duplicates()
+dups = test_data_df.duplicated()
+if dups.any():
+    print("Duplicates found on test data")
+    test_data_df = test_data_df.drop_duplicates()
+
+# Training data
 x_train = train_data[:,:in_col] #input values
 y_train = train_data[:,in_col:] #output values
 
-#Testing data
+# Testing data
 x_test = test_data[:,:in_col] #input values
 y_test = test_data[:,in_col:] #output values
 
-#Normalize the data
+# Normalize the data
 x_trans = StandardScaler()
 x_trans.fit(x_train)
 y_trans = StandardScaler()
@@ -88,8 +102,7 @@ y_train = y_trans.transform(y_train)
 y_test = y_trans.transform(y_test)
 x_test = x_trans.transform(x_test)
 
-# %%
-########################################################    Hyperparameters    #################################################################
+# %% Hyperparameters
 
 #Specify NN hyperparameters
 activation = 'tanh' #Activation function
@@ -104,10 +117,9 @@ num_inputs = x_train.shape[1] #Number of inputs
 num_outputs = y_train.shape[1] #Number of outputs
 layers = [num_inputs] + 4 * [100] + [num_outputs] #NN architecture
 
-# %%
-############################################################    NN model    #####################################################################
+# %% NN model
 
-#Build the NN structure 
+# Build the NN structure 
 model = Sequential()
 model.add(Dense(layers[1], input_shape=(layers[0],), kernel_initializer=initial_weights,
                 bias_initializer=initial_weights, activation=activation))
@@ -120,8 +132,8 @@ model.add(Dense(layers[-1], activity_regularizer=reg))
 model.compile(optimizer=opt, loss='mse') #Complile the model
 
 #save best model
-checkpoint = ModelCheckpoint('weights/1-173_&32000epochs_weights.h5',monitor='loss',      #model checkpointing
-                            verbose=1, save_best_only=True, mode='min',
+checkpoint = ModelCheckpoint('weights/weights.h5',monitor='loss',      #model checkpointing
+                            verbose=0, save_best_only=True, mode='min',
                             save_freq='epoch', period=1)
 csv_logger = CSVLogger('training.log')    #Saves the training loss values
 earlystopping = EarlyStopping(monitor='loss', min_delta=0, patience=10000,  #earlystopping
@@ -133,11 +145,11 @@ r = model.fit(x_train,y_train, epochs=epochs,
               batch_size=batch_size,
               callbacks=[checkpoint,csv_logger,earlystopping]) 
 
-# Prediction
+# %%Prediction
 y_pred = model.predict(x_test) #uses NN weights saved at last epoch
 
 # load weights
-model.load_weights("weights/1-173_&32000epochs_weights.h5") #NN weights saved from epoch with lowest loss value
+model.load_weights("weights/weights.h5") #NN weights saved from epoch with lowest loss value
 model.compile(optimizer=opt, loss='mse')
 y_pred_best = model.predict(x_test) #prediction when loss value is lowest during training
 
@@ -205,3 +217,5 @@ print(min(r.history['loss']))
 np.savez('saved_variables',y_pred=y_pred,y_pred_best=y_pred_best)
 loaded_variables = np.load('saved_variables.npz')
 print(loaded_variables.files)
+# %%
+
--- src/TT_post_process.py
+++ src/TT_post_process.py
@@ -1,105 +1,243 @@
+# Author: Al Shahriar
+# June 28 2023
+# %%
 import numpy as np
 import pandas as pd
-
-# %%
-class Compressor:
-    # compressor information
-    def __init__(self):
-        # create stages
-        self.s1 = self.Stage()
-        #self.s2 = self.Stage()
-        self.volute_throat_area = 1.0
-        self.volute_exit_pipe_dia = 1.0
-        self.rpm = 10000.0
-        self.mass_flow_rate = 1.0
-        self.isetropic_efficiency = 0.8
-        self.pressure_ratio = 2.0
-        self.power = 1
-    # stage information
-    class Stage:
-        def __init__(self):
-            # create stage information
-            self.TE_blade_ang_hub = []
-            self.TE_blade_ang_tip = []
-            self.inlet_inclination_ang = 1.0
-            self.number_of_main_blades = 1
-            self.LE_Clearance = []
-            self.TE_Clearance = []
-            self.shroud_rad = 1.0
-            self.inlet_hub_rad = 1.0
-            self.impeller_outlet_rad = 1.0
-            self.impeller_outlet_width = 1.0
-            self.Rpin = []
-            self.Bpin = []
-            self.outlet_avg_rad = 1.0
-
-cpr = Compressor() 
-
-# %%
+from compressor import Compressor
+# %% Reading the data
+# Inputs to TT
 batch_number = 0
-data_in = pd.read_csv("../tt_input/batch_"+str(batch_number)+".csv").T
+if 0:
+    # inside TT, right click on the paramatric study table, select export as csv
+    data_in = pd.read_csv("../tt_input/batch_"+str(batch_number)+".csv").T
+    read_method = 0
+else:
+    # if created using py script
+    data_in = pd.read_pickle("random_parameters.pkl")
+    read_method = 1
+# Outputs of TT
 data_out_raw = pd.read_csv("../tt_output/batch_"+str(batch_number)+".csv",index_col='Parameter').T
-# Rows to remove
+# %% 
+# Removing unnecessary rows
 data_out = data_out_raw.drop(["Object","Unit","Run1_dp"],axis = 0)
-# %%
 
-cpr.rpm = data_out["RPM"].values
-cpr.mass_flow_rate = data_out["Inlet mass flow"].values
-cpr.isetropic_efficiency = data_out["Isentropic machine efficiency (T-S)"].values
-cpr.pressure_ratio = data_out["Machine pressure ratio (T-S)"].values
-cpr.power = data_out["Machine power"].values
+# renaming the column - one column has duplicate name
+data_out_cols = data_out.columns.tolist()
+i_pressure_ratio_out = 0
+i_machine_power = 1
+i_efficiency_out = 2
+i_mass_flow_rate_out = 3
+i_RPM_out = 5
+i_n_main_blades_out = 6
+i_TE_blade_ang_hub_out = 7
+i_TE_blade_ang_tip_out = 8
+i_inclination_angle_out = 9
+i_hub_radius_out = 10
+i_shroud_radius_out = 11
+i_impl_radius_out = 12
+i_impl_width_out = 13
+i_diff_radius_out = 14
+i_diff_width_out = 15
+i_throat_area_out = 16
+i_pipe_diameter_out = 17
+i_Rpin_out = 18
+i_Bpin_out = 19
+i_LE_Clearance_out = 20;
+i_TE_Clearance_out = 21;
 
-cpr.s1.inlet_inclination_ang = data_out["Inclination angle"].values
-cpr.s1.number_of_main_blades = data_out["Number of main blades"].values
-cpr.s1.shroud_rad = data_out["Shroud radius"].values
-cpr.s1.inlet_hub_rad = data_out["Hub radius"].values
-cpr.s1.impeller_outlet_rad = data_out["Outlet radius (avg)"].values
-cpr.s1.impeller_outlet_width = data_out["Outlet width"].values
+TE_blade_ang_hub_txt ='TE Blade Angle Hub'
+TE_blade_ang_tip_txt ='TE Blade Angle Tip'
+if data_out_cols[i_TE_blade_ang_hub_out] == 'TE blade angle': data_out_cols[i_TE_blade_ang_hub_out] = TE_blade_ang_hub_txt
+if data_out_cols[i_TE_blade_ang_tip_out] == 'TE blade angle': data_out_cols[i_TE_blade_ang_tip_out] = TE_blade_ang_tip_txt
+if data_out_cols[i_impl_radius_out] == 'Outlet radius (avg)': data_out_cols[i_impl_radius_out] = "Impeller radius"
+if data_out_cols[i_impl_width_out] == 'Outlet width': data_out_cols[i_impl_width_out] = "Impeller width"
+if data_out_cols[i_diff_radius_out] == 'Avg. radius': data_out_cols[i_diff_radius_out] = "Diffuser radius"
+if data_out_cols[i_diff_width_out] == 'Width': data_out_cols[i_diff_width_out] = "Diffuser width"
 
-cpr.volute_throat_area = data_out["Throat area"].values
-cpr.volute_exit_pipe_dia = data_out["Exit pipe diameter"].values
+data_out.columns=data_out_cols
+data_out_cols = data_out.columns.tolist()
+pressure_ratio_txt = data_out_cols[i_pressure_ratio_out]
+efficiency_txt = data_out_cols[i_efficiency_out]
+impl_radius_txt = data_out_cols[i_impl_radius_out]
+impl_width_txt = data_out_cols[i_impl_width_out]
+diff_radius_txt = data_out_cols[i_diff_radius_out]
+diff_width_txt = data_out_cols[i_diff_width_out]
 
+# %% Data indexing
+# TT does not return the results for the failed cases in the parametric study
 # column number in the csv - input for TT
-TE_blade_ang_hub_index = 0;
-TE_blade_ang_tip_index = 1;
-LE_Clearance_index = 4;
-TE_Clearance_index = 5;
-Rpin_Rin_index = 12;
-Bpin_Rin_index = 13;
+i_TE_blade_ang_hub_in = 0;
+i_TE_blade_ang_tip_in = 1;
+i_LE_Clearance_in = 4;
+i_TE_Clearance_in = 5;
+i_Rpin_by_Rin_in = 12;
+i_Bpin_by_Rin_in = 13;
 
-nRows = len(data_out.index)
-for irow in range(0,nRows):
-    row = data_out.index[irow]
-    nL = len(row)
-    case_number = int(row[18:nL])
-    #print(case_number)
-    cpr.s1.Rpin.append(data_in.loc[str(case_number)].at[Rpin_Rin_index]*cpr.s1.impeller_outlet_rad[irow])
-    cpr.s1.Bpin.append(data_in.loc[str(case_number)].at[Bpin_Rin_index]*cpr.s1.impeller_outlet_width[irow])
-    cpr.s1.LE_Clearance.append(data_in.loc[str(case_number)].at[LE_Clearance_index])
-    cpr.s1.TE_Clearance.append(data_in.loc[str(case_number)].at[TE_Clearance_index])
-    cpr.s1.TE_blade_ang_hub.append(data_in.loc[str(case_number)].at[TE_blade_ang_hub_index])
-    cpr.s1.TE_blade_ang_tip.append(data_in.loc[str(case_number)].at[TE_blade_ang_tip_index])
+temp_Rpin = [];
+temp_Bpin = [];
+temp_LE_Clearance = [];
+temp_TE_Clearance = [];
+temp_TE_blade_ang_hub = [];
+temp_TE_blade_ang_tip = [];
 
-# make pretty
-df = pd.DataFrame({
-    "TE_blade_ang_hub": cpr.s1.TE_blade_ang_hub,
-    "TE_blade_ang_tip": cpr.s1.TE_blade_ang_tip,
-    "inlet_inclination_ang": cpr.s1.inlet_inclination_ang,
-    "number_of_main_blades": cpr.s1.number_of_main_blades,
-    "LE_Clearance": cpr.s1.LE_Clearance,
-    "TE_Clearance": cpr.s1.TE_Clearance,
-    "shroud_rad": cpr.s1.shroud_rad,
-    "inlet_hub_rad": cpr.s1.inlet_hub_rad,
-    "impeller_outlet_rad": cpr.s1.impeller_outlet_rad,
-    "impeller_outlet_width": cpr.s1.impeller_outlet_width,
-    "Rpin": cpr.s1.Rpin,
-    "Bpin": cpr.s1.Bpin,
-    "outlet_avg_rad": cpr.s1.outlet_avg_rad,
-    "volute_throat_area": cpr.volute_throat_area,
-    "volute_exit_pipe_dia": cpr.volute_exit_pipe_dia,
-    "rpm": cpr.rpm,
-    "mass_flow_rate": cpr.mass_flow_rate,
-    "isetropic_efficiency": cpr.isetropic_efficiency,
-    "pressure_ratio": cpr.pressure_ratio,    
-    "power": cpr.power
-})
+nRows_out = len(data_out.index)
+nRows_in = len(data_in.index)
+for irow_out in range(0,nRows_out):
+    row_out = data_out.index[irow_out]
+    case_number_txt = (row_out[18:len(row_out)])
+    case_number = int(case_number_txt)
+    if read_method == 0:
+        temp_Rpin.append(data_in.loc[case_number_txt].at[i_Rpin_by_Rin_in]*data_out[impl_radius_txt][irow_out])
+        temp_Bpin.append(data_in.loc[case_number_txt].at[i_Bpin_by_Rin_in]*data_out[impl_width_txt][irow_out])
+        temp_LE_Clearance.append(data_in.loc[case_number_txt].at[i_LE_Clearance_in])
+        temp_TE_Clearance.append(data_in.loc[case_number_txt].at[i_TE_Clearance_in])
+        temp_TE_blade_ang_hub.append(data_in.loc[case_number_txt].at[i_TE_blade_ang_hub_in])
+        temp_TE_blade_ang_tip.append(data_in.loc[case_number_txt].at[i_TE_blade_ang_tip_in])
+    else:
+        temp_Rpin.append(data_in.iloc[case_number].at["Rpin_s1"]*data_out[impl_radius_txt][irow_out])
+        temp_Bpin.append(data_in.iloc[case_number].at["Bpin_s1"]*data_out[impl_width_txt][irow_out])
+        temp_LE_Clearance.append(data_in.iloc[case_number].at["LE_Clearance_s1"])
+        temp_TE_Clearance.append(data_in.iloc[case_number].at["TE_Clearance_s1"])
+        temp_TE_blade_ang_hub.append(data_in.iloc[case_number].at["TE_blade_ang_hub_s1"])
+        temp_TE_blade_ang_tip.append(data_in.iloc[case_number].at["TE_blade_ang_tip_s1"])
+        
+
+data_out["Rpin"] = temp_Rpin
+data_out["Bpin"] = temp_Bpin
+data_out["LE Clearance"] = temp_LE_Clearance
+data_out["TE Clearance"] = temp_TE_Clearance
+data_out[TE_blade_ang_hub_txt] = temp_TE_blade_ang_hub
+data_out[TE_blade_ang_tip_txt] = temp_TE_blade_ang_tip
+data_out_cols = data_out.columns.tolist()
+# %% Data cleaning
+
+pressure_ratio_min = 1.0;
+pressure_ratio_max = 5.0;
+
+efficiency_min = 0.5;
+efficiency_max = 1.0;
+
+nRows_out = len(data_out.index)
+
+cases_to_remove_eta_high = [];
+cases_to_remove_eta_low = [];
+cases_to_remove_pr_high = [];
+cases_to_remove_pr_low = [];
+flag_rm_eta = np.zeros(((nRows_out,1)),dtype = bool);
+flag_rm_pr = np.zeros(((nRows_out,1)),dtype = bool);
+
+for irow_out in range(0,nRows_out):
+    eta = data_out[efficiency_txt][irow_out]
+    flag = bool(False)
+    if eta>efficiency_max:
+        cases_to_remove_eta_high.append(data_out.index[irow_out])
+        flag_rm_eta[irow_out] = bool(True)
+    if eta<efficiency_min:
+        cases_to_remove_eta_low.append(data_out.index[irow_out])
+        flag_rm_eta[irow_out] = bool(True)    
+
+for irow_out in range(0,nRows_out):
+    pr = data_out[pressure_ratio_txt][irow_out]
+    if pr>pressure_ratio_max:
+        cases_to_remove_pr_high.append(data_out.index[irow_out])
+        flag_rm_pr[irow_out] = bool(True)
+    if pr<pressure_ratio_min:
+        cases_to_remove_pr_low.append(data_out.index[irow_out])
+        flag_rm_pr[irow_out] = bool(True)
+        
+cases_to_remove_temp = cases_to_remove_eta_high + cases_to_remove_eta_low + \
+    cases_to_remove_pr_high + cases_to_remove_pr_low
+cases_to_remove = [*set(cases_to_remove_temp)]
+
+flag_rm = np.logical_or(flag_rm_eta,flag_rm_pr)
+data_out["Bad Samples"] = flag_rm
+data_out_filtered = data_out[data_out["Bad Samples"] == False]
+data_out_bad_samples = data_out[data_out["Bad Samples"] == True]
+
+# %% Seperating the testing and training data
+nRows = len(data_out_filtered)
+nTrain = int(0.8*nRows)
+
+data_ordered = data_out_filtered.iloc[:, \
+                                     [i_mass_flow_rate_out,i_RPM_out,i_n_main_blades_out, \
+                                      i_TE_blade_ang_hub_out, i_TE_blade_ang_tip_out, \
+                                      i_LE_Clearance_out,i_TE_Clearance_out, \
+                                      i_inclination_angle_out, i_hub_radius_out, i_shroud_radius_out, \
+                                      i_impl_radius_out, i_impl_width_out, i_Rpin_out, i_Bpin_out, \
+                                      i_diff_radius_out, i_diff_width_out, \
+                                      i_throat_area_out, i_pipe_diameter_out, \
+                                      i_pressure_ratio_out,i_machine_power,i_efficiency_out]]
+data_train = data_ordered.iloc[:nTrain,:]    
+data_test = data_ordered.iloc[nTrain+1:,:]
+data_train.to_csv('../training_data/train_parameters.csv', index=False)
+data_test.to_csv('../testing_data/test_parameters.csv', index=False)
+data_train.to_pickle("../training_data/train_parameters.pkl")
+data_test.to_pickle("../testing_data/test_parameters.pkl")
+# %% Trying something with Compressor class
+# Not necessary - optional
+cpr = Compressor() 
+if 1:
+    # TT does not return the results for the failed cases in the parametric study
+    cpr.rpm = data_out["RPM"].values
+    cpr.mass_flow_rate = data_out["Inlet mass flow"].values
+    cpr.isetropic_efficiency = data_out["Isentropic machine efficiency (T-S)"].values
+    cpr.pressure_ratio = data_out["Machine pressure ratio (T-S)"].values
+    cpr.power = data_out["Machine power"].values
+
+    cpr.s1.inlet_inclination_ang = data_out["Inclination angle"].values
+    cpr.s1.number_of_main_blades = data_out["Number of main blades"].values
+    cpr.s1.shroud_rad = data_out["Shroud radius"].values
+    cpr.s1.inlet_hub_rad = data_out["Hub radius"].values
+    cpr.s1.impeller_outlet_rad = data_out[impl_radius_txt].values
+    cpr.s1.impeller_outlet_width = data_out[impl_width_txt].values
+
+    cpr.volute_throat_area = data_out["Throat area"].values
+    cpr.volute_exit_pipe_dia = data_out["Exit pipe diameter"].values
+
+    nRows_out = len(data_out.index)
+    nRows_in = len(data_in.index)
+    for irow_out in range(0,nRows_out):
+        row_out = data_out.index[irow_out]
+        case_number_txt = (row_out[18:len(row_out)])
+        if read_method == 0:
+            cpr.s1.Rpin.append(data_in.loc[case_number_txt].at[i_Rpin_by_Rin_in]*cpr.s1.impeller_outlet_rad[irow_out])
+            cpr.s1.Bpin.append(data_in.loc[case_number_txt].at[i_Bpin_by_Rin_in]*cpr.s1.impeller_outlet_width[irow_out])
+            cpr.s1.LE_Clearance.append(data_in.loc[case_number_txt].at[i_LE_Clearance_in])
+            cpr.s1.TE_Clearance.append(data_in.loc[case_number_txt].at[i_TE_Clearance_in])
+            cpr.s1.TE_blade_ang_hub.append(data_in.loc[case_number_txt].at[i_TE_blade_ang_hub_in])
+            cpr.s1.TE_blade_ang_tip.append(data_in.loc[case_number_txt].at[i_TE_blade_ang_tip_in])
+        else:
+            case_number = int(case_number_txt)
+            cpr.s1.Rpin.append(data_in.iloc[case_number].at["Rpin_s1"]*cpr.s1.impeller_outlet_rad[irow_out])
+            cpr.s1.Bpin.append(data_in.iloc[case_number].at["Bpin_s1"]*cpr.s1.impeller_outlet_width[irow_out])
+            cpr.s1.LE_Clearance.append(data_in.iloc[case_number].at["LE_Clearance_s1"])
+            cpr.s1.TE_Clearance.append(data_in.iloc[case_number].at["TE_Clearance_s1"])
+            cpr.s1.TE_blade_ang_hub.append(data_in.iloc[case_number].at["TE_blade_ang_hub_s1"])
+            cpr.s1.TE_blade_ang_tip.append(data_in.iloc[case_number].at["TE_blade_ang_tip_s1"])
+        
+    # make pretty
+    data_out_final = pd.DataFrame({
+        "TE_blade_ang_hub": cpr.s1.TE_blade_ang_hub,
+        "TE_blade_ang_tip": cpr.s1.TE_blade_ang_tip,
+        "inlet_inclination_ang": cpr.s1.inlet_inclination_ang,
+        "number_of_main_blades": cpr.s1.number_of_main_blades,
+        "LE_Clearance": cpr.s1.LE_Clearance,
+        "TE_Clearance": cpr.s1.TE_Clearance,
+        "shroud_rad": cpr.s1.shroud_rad,
+        "inlet_hub_rad": cpr.s1.inlet_hub_rad,
+        "impeller_outlet_rad": cpr.s1.impeller_outlet_rad,
+        "impeller_outlet_width": cpr.s1.impeller_outlet_width,
+        "Rpin": cpr.s1.Rpin,
+        "Bpin": cpr.s1.Bpin,
+        "outlet_avg_rad": cpr.s1.outlet_avg_rad,
+        "volute_throat_area": cpr.volute_throat_area,
+        "volute_exit_pipe_dia": cpr.volute_exit_pipe_dia,
+        "rpm": cpr.rpm,
+        "mass_flow_rate": cpr.mass_flow_rate,
+        "isetropic_efficiency": cpr.isetropic_efficiency,
+        "pressure_ratio": cpr.pressure_ratio,    
+        "power": cpr.power
+    })
+
+
+
